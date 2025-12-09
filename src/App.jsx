55ac6ffe3418216f7ea09ffe6c8bc2ac998c9aba
Aslin232 <assasingod120@gmail.com>
2025-12-09 13:35:19 +0530

    Implement dynamic document title updates
    Added TitleUpdater component to manage document title based on route.

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,34 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { useEffect } from "react";
 import Home from "./pages/Home";
 import GuppyDetail from "./pages/GuppyDetail";
 import "./App.css";
 
+function TitleUpdater() {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.pathname === "/") {
+      document.title = "Aquavibe - Home";
+    } else if (location.pathname.startsWith("/guppy/")) {
+      document.title = "Aquavibe - Guppy Details";
+    } else {
+      document.title = "Aquavibe";
+    }
+  }, [location]);
+
+  return null;
+}
+
 function App() {
   return (
     <BrowserRouter>
+      <TitleUpdater />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/guppy/:id" element={<GuppyDetail />} />
-      
       </Routes>
     </BrowserRouter>
-    
   );
 }
 
